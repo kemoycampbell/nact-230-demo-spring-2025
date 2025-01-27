d88f8076271633e42a2f0b1a83ff5d90d661b8ab
Kemoy Campbell <kscics@rit.edu>
2025-01-27 13:05:10 -0500

    example of writing a file on vsc

---
+++ square.py
@@ -0,0 +1,28 @@
+"""
+Purpose:
+    This is a program that use python
+    turtle to draw a square.
+
+Author:
+    Kemoy Campbell (kscics@rit.edu)
+Date:01/27/2025
+"""
+import turtle
+
+#start by setting the turtle to face up
+turtle.left(90)
+#draw the top of the sqare
+turtle.forward(100)
+turtle.right(90)
+#draw the right side of the square
+turtle.forward(100)
+turtle.right(90)
+#draw the bottom side of the square
+turtle.forward(100)
+turtle.right(90)
+#draw the left side of the square
+turtle.forward(100)
+turtle.right(90)
+
+#keep the windows open
+turtle.done()
